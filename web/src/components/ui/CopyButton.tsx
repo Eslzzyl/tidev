c33775162a9a3e7f7bad8dacb1b0b9084d780693
Eslzzyl <eslzzyl@163.com>
2026-05-03 00:05:40 +0800

    chore(web): optimize copy button style

--- web/src/components/ui/CopyButton.tsx
+++ web/src/components/ui/CopyButton.tsx
@@ -48,7 +48,7 @@ export function CopyButton({ content }: CopyButtonProps) {
             strokeLinecap="round"
             strokeLinejoin="round"
             strokeWidth={2}
-            d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h14a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
+            d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2"
           />
         </svg>
       )}
