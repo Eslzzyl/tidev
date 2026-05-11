5c664d7e4d61b9862fb3dcfc9a486e73c234c1b3
Eslzzyl <eslzzyl@163.com>
2026-05-11 22:39:04 +0800

    chore: improve gruvbox_light theme color

--- src/theme/gruvbox_light.rs
+++ src/theme/gruvbox_light.rs
@@ -1,14 +1,14 @@
-use ratatui::style::Color;
 use crate::theme::{ThemeName, ThemePalette};
+use ratatui::style::Color;
 
 impl ThemePalette {
     pub fn gruvbox_light() -> Self {
         Self {
             name: ThemeName::GruvboxLight,
-            background: Color::Rgb(251, 241, 199),
-            panel: Color::Rgb(235, 219, 178),
-            panel_alt: Color::Rgb(213, 196, 161),
-            panel_light: Color::Rgb(189, 174, 147),
+            background: Color::Rgb(255, 249, 227),
+            panel: Color::Rgb(245, 233, 203),
+            panel_alt: Color::Rgb(240, 231, 214),
+            panel_light: Color::Rgb(240, 231, 214),
             text: Color::Rgb(60, 56, 54),
             muted: Color::Rgb(146, 131, 116),
             border: Color::Rgb(189, 174, 147),
